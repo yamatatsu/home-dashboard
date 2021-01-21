8ae74d8c3339152dc92eb8d404254dd8d499a269
yamatatsu <the.kinnikuman@gmail.com>
2021-01-22 01:26:33 +0900

    remove cloudfront

--- packages/browser-app/src/constants.ts
+++
@@ -1,9 +0,0 @@
-const API_ORIGIN = process.env.API_ORIGIN;
-export const DEV = process.env.NODE_ENV === "development";
-
-export const SIGN_UP_CHALLENGE_URL = API_ORIGIN + "/auth/signUpChallenge";
-export const SIGN_UP_URL = API_ORIGIN + "/auth/signUp";
-export const SIGN_IN_CHALLENGE_URL = API_ORIGIN + "/auth/signInChallenge";
-export const SIGN_IN_URL = API_ORIGIN + "/auth/signIn";
-
-export const REMO_EVENTS_URL = API_ORIGIN + "/remoEvents";
--- packages/cdk/stacks/index.ts
+++ packages/cdk/stacks/index.ts
@@ -2,11 +2,9 @@ import "source-map-support/register";
 import * as dotenv from "dotenv";
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
-import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 import { DynamodbStack } from "./Dynamodb";
 import { FetchRemoApi } from "./FetchRemoApi";
 import { WebApi } from "./WebApi";
-import { WebFront } from "./WebFront";
 
 dotenv.config();
 
@@ -47,7 +45,4 @@ new WebApi(app, "WebApi", {
   allowOrigins: ["https://home.yamatatsu.dev"],
   rpId: "home.yamatatsu.dev",
   dev: false,
-});
-new WebFront(app, "WebFront", {
-  source: s3Deployment.Source.asset(`${__dirname}/../../browser-app/dist`),
 });
